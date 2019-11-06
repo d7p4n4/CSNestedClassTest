fefcde74de31966d2eafd57bd600df19070768f6
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-06 15:08:13 +0000

    Add project files.

---
+++ CSNestedClassTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSNestedClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var ctx = new Context())
+            {
+                Student student = new Student()
+                {
+                    EnrollmentDate = DateTime.Today,
+                    Identity = new Student.Person("Jack", DateTime.Parse("1996-10-23"))
+                };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CSNestedClassTest/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSNestedClassTest
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+        public Person Identity { get; set; }
+
+
+        public class Person
+        {
+
+            public Person(string fatherName, DateTime birthDate)
+            {
+                FatherName = fatherName;
+                BirthDate = birthDate;
+            }
+
+            public string FatherName { get; set; }
+            public DateTime BirthDate { get; set; }
+
+        }
+    }
+}
